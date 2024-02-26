250db2ba3283ba3b371bb8120eb11ee6cf5b3954
hafeyang <hafeyang@gmail.com>
2024-02-26 13:10:17 +0800

    feat: add proxy

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const http = require("http");
-
-// const hostname = "127.0.0.1";
-// const port = 3000;
+const httpProxy = require("http-proxy");
+const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, js!\n");
+  if (/^\/api\/v1\/public\//g.test(req.url) || /^\/api\/v1\/private\//g.test(req.url)) {
+    proxy.web(req, res, { target: "https://dev.dex2.xyz", secure: false, changeOrigin: true });
+  } else {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello, js!\n");
+  }
 });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
 
 module.exports = server;
