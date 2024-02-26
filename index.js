419f6640c2349e0efdb1b4f032554eca1a87100f
hafeyang <hafeyang@gmail.com>
2024-02-26 19:08:13 +0800

    feat: rewrites

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const httpProxy = require("http-proxy");
 const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer((req, res) => {
-  if (/^\/api\/v1\/public\//g.test(req.url) || /^\/api\/v1\/private\//g.test(req.url)) {
-    proxy.web(req, res, { target: "https://dev.dex2.xyz", secure: false, changeOrigin: true });
-  } else {
+  // if (/^\/api\/v1\/public\//g.test(req.url) || /^\/api\/v1\/private\//g.test(req.url)) {
+  //   proxy.web(req, res, { target: "https://dev.dex2.xyz", secure: false, changeOrigin: true });
+  // } else {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("Hello, js!\n");
-  }
+  // }
 });
 
 module.exports = server;
